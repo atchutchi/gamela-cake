d6ebc764645642e387d938eca6c4d2aed739ac8f
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-11-04 06:55:18 +0000

    Add allowed_host from heroku app

--- gamelacake/settings.py
+++ gamelacake/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-atchutchi-gamelacake-8jbxmfmnqei.ws-eu105.gitpod.io', '', 'localhost']
+ALLOWED_HOSTS = ['8000-atchutchi-gamelacake-8jbxmfmnqei.ws-eu105.gitpod.io', 'gamela-cake-5a01bc199f23.herokuapp.com', 'localhost']
 
 
 # Application definition
