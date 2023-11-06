3a1d7c247137ea8318b61c1109ab9991c3c4b533
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-11-06 05:28:11 +0000

    Add databese models from the diagram

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # Use Django's built-in methods for password handling
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Add any additional fields here
+
+    def __str__(self):
+        return self.username
+
+class Cake(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = CloudinaryField('image', default='placeholder')  # This will use Cloudinary for image storage
+
+    def __str__(self):
+        return self.name
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    party_size = models.IntegerField()
+    special_request = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Reservation for {self.user.username} on {self.date} at {self.time}"
+
+class Order(models.Model):
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    cake = models.ForeignKey(Cake, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} for {self.reservation}"
+
+# Add any additional models here
