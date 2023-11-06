5ad819c46023efacf28835eff4f1078b6a9380f1
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-11-06 05:35:48 +0000

    Add comments to the moodels.py

--- booking/models.py
+++ booking/models.py
@@ -2,28 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+# Custom User model extending the default Django User
 class User(models.Model):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)  # Use Django's built-in methods for password handling
+    password = models.CharField(max_length=128)
     date_joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     # Add any additional fields here
-
+    # String representation of the User model
     def __str__(self):
         return self.username
 
+# Cake model to store cake details
 class Cake(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = CloudinaryField('image', default='placeholder')  # This will use Cloudinary for image storage
 
+    # String representation of the Cake model
     def __str__(self):
         return self.name
 
+# Reservation model to store booking details
 class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
@@ -32,15 +36,18 @@ class Reservation(models.Model):
     special_request = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # String representation of the Reservation model
     def __str__(self):
         return f"Reservation for {self.user.username} on {self.date} at {self.time}"
 
+# Order model to store order details associated with a reservation
 class Order(models.Model):
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
     cake = models.ForeignKey(Cake, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # String representation of the Order model
     def __str__(self):
         return f"Order {self.id} for {self.reservation}"
 
