cc447d1773a0e6c60139aea071916b3b56312677
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-11-18 11:32:49 +0000

    Added feature to display featured cakes on homepage

--- booking/views.py
+++ booking/views.py
@@ -20,6 +20,13 @@ from .forms import ReservationForm
 class HomeView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Seleciona os primeiros 4 bolos (ajuste a query conforme necessário)
+        featured_cakes = Cake.objects.all()[:4]
+        context['cakes'] = featured_cakes
+        return context
+
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
